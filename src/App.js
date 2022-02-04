4e77644a4ae457d8a82b5026049878abb69df4c0
Desalegn Birhanu <brightnet12@gmail.com>
2022-02-04 14:23:11 +0300

    Adding files to githunb page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { SearchBox } from './components/search-box/search-box.component';
+import { CardList } from './components/card-lis/card-list.component';
 
-function App() {
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>response.json())
+    .then(users=>this.setState({monsters:users}))
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
+  render(){
+    const {monsters, searchField} = this.state
+    const filteredMonsters = monsters.filter(monster=>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+    return (      
+      <div className="App">
+      <h1>Monsters Rolodex</h1>
+      <SearchBox 
+        placeholder='Search Monsters' 
+        handleChange={this.handleChange}
+      />      
+      <CardList monsters={filteredMonsters}></CardList>        
+      </div>
+    );
+  }
+}
+
+
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +61,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
